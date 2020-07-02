bf9cd6730177befb77dac8e3246e4944ee7d5328
Zombieteer <aryan.nigam1996@gmail.com>
2020-07-02 13:13:43 +0530

    Weatherati

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  home: Scaffold(
+    appBar: AppBar(
+      title: Text('My fist App'),
+    ),
+    body: Center(
+      child: Text('hello guys')),
+  ),
+));
